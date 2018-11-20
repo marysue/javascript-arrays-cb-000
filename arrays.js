e104e116d85fddcf6571d066d415ff577cde69e1
Mary Lark <mary@marylark.com>
2018-11-20 23:49:10 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -13,19 +13,31 @@ function destructivelyAddElementToBeginningOfArray(arr, elt)
   return arr;
 }
 
+<<<<<<< HEAD
 function addElementToEndOfArray(arr, elt){
+=======
+function addElementToendOfArray(arr, elt){
+>>>>>>> fa78a02aff67b8117531819f9ea2ff0f863080d2
   //do NOT alter the original array
   //add the elt to the end of the array
   return [...arr, elt]
 
 }
 
+<<<<<<< HEAD
 function destructivelyAddElementToEndOfArray (arr, elt)
+=======
+function destructivelyAddElementToendOfArray (arr, elt)
+>>>>>>> fa78a02aff67b8117531819f9ea2ff0f863080d2
 {
   //alter the original array
   //push - add elt to end of array - destructively
   arr.push(elt);
+<<<<<<< HEAD
   return arr;
+=======
+  return arr
+>>>>>>> fa78a02aff67b8117531819f9ea2ff0f863080d2
 }
 function accessElementInArray(arr, idx)
 {
@@ -38,6 +50,7 @@ function destructivelyRemoveElementFromBeginningOfArray(arr)
   //removes the first element
   //return the entire array : mutated
   // use slice method slice(elt#) where 1 = first elt of array :
+<<<<<<< HEAD
   arr.shift();
   return arr;
 }
@@ -56,3 +69,8 @@ function removeElementFromBeginningOfArray(arr) {
 }
 
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+=======
+  arr = arr.slice(1);
+  return arr;
+}
+>>>>>>> fa78a02aff67b8117531819f9ea2ff0f863080d2
